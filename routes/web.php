8d308c5366064006ec00c164ccc7ab49b28dea38
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-11-10 12:33:25 +0545

    email verification added

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/private', [HomeController::class, 'private']);
-Auth::routes();
+Auth::routes(['verify'=>true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
